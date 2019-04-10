b256232650301746ecef87294731450103521438
jlauener <jlauener@student.ethz.ch>
2019-04-10 20:35:00 +0200

    better naming

--- lowlvl_communication/src/ExampleNode.cpp
+++ lowlvl_communication/src/ExampleNode.cpp
@@ -3,24 +3,24 @@
 namespace lowlvl_communication {
 
 ExampleNode::ExampleNode(any_node::Node::NodeHandlePtr nh): any_node::Node(nh), 
-   as_device_state_(*this->nh_, "DeviceState", boost::bind (&ExampleNode::deviceStateCb, this, _1),false),
-   as_homing_(*this->nh_.get(), "Homing" , boost::bind(&ExampleNode::homingCb, this, _1),false)    
+   deviceStateActionServer_(*nh, "DeviceState", boost::bind (&ExampleNode::deviceStateCallback, this, _1),false),
+   homingActionServer_(*nh, "Homing" , boost::bind(&ExampleNode::homingCallback, this, _1),false)
 {
   //Action Server
-  as_device_state_.start();
-  as_homing_.start();
-
+  deviceStateActionServer_.start();
+  homingActionServer_.start();
 }
 
 bool ExampleNode::init()
 {
   constexpr unsigned int defaultQueueSize = 1;
   //Publisher
-  pub_joint_state_= advertise<varileg_msgs::ExtendedJointStates>("joint_state","joint_state",1);
-  pub_device_state_= advertise<varileg_msgs::ExtendedDeviceStates>("device_state","device_state",1);
+  jointStatesPublisher_= advertise<varileg_msgs::ExtendedJointStates>("joint_state","joint_state",1);
+  deviceStatePublisher_= advertise<varileg_msgs::ExtendedDeviceStates>("device_state","device_state",1);
 
   //Subscriber
-  sub_joint_trajectory_ = subscribe("joint_trajectory", "joint_trajectory", defaultQueueSize, &ExampleNode::jointTrajectoryCb, this);
+  jointTrajectoriesSubscriber_ = subscribe("joint_trajectory", "joint_trajectory", defaultQueueSize,
+                                          &ExampleNode::jointTrajectoriesCallback, this);
 
 
   constexpr double defaultWorkerTimeStep = 3.0;
@@ -37,9 +37,9 @@ bool ExampleNode::init()
 }
 
 //Subscriber
-void ExampleNode::jointTrajectoryCb (const varileg_msgs::ExtendedJointTrajectoriesConstPtr &msg)
+void ExampleNode::jointTrajectoriesCallback(const varileg_msgs::ExtendedJointTrajectoriesConstPtr &msg)
 {
-  MELO_INFO("Received jointTrajectoryCb Topic");
+  MELO_INFO("Received jointTrajectoriesCallback Topic");
   msg->name[0];
   msg->position[0];
   /* @Jonas
@@ -47,39 +47,39 @@ void ExampleNode::jointTrajectoryCb (const varileg_msgs::ExtendedJointTrajectori
   */
 }
 //Action Server
-void ExampleNode::deviceStateCb (const varileg_msgs::DeviceStateGoalConstPtr &goal)
+void ExampleNode::deviceStateCallback(const varileg_msgs::DeviceStateGoalConstPtr &goal)
 {
-  MELO_INFO("Received deviceStateCb Action");
+  MELO_INFO("Received deviceStateCallback Action");
   goal->target_device_state;
   
   /* @Jonas
-  feedback_device_state_.current_device_state = [...]; 
-  as_device_state_.publishFeedback(feedback_device_state_);
-  result_device_state_.successful = [...];
-  result_device_state_.reached_device_state = [...];
+  deviceStateFeedback_.current_device_state = [...];
+  deviceStateActionServer_.publishFeedback(deviceStateFeedback_);
+  deviceStateResult_.successful = [...];
+  deviceStateResult_.reached_device_state = [...];
   */
   
-  as_device_state_.setSucceeded(result_device_state_);
+  deviceStateActionServer_.setSucceeded(deviceStateResult_);
 }
 //Action Server
-void ExampleNode::homingCb (const varileg_msgs::HomingGoalConstPtr &goal)
+void ExampleNode::homingCallback(const varileg_msgs::HomingGoalConstPtr &goal)
 {
-  MELO_INFO("Received homingCb Action");
+  MELO_INFO("Received homingCallback Action");
 
   /* @Jonas
   goal -> ?
-  feedback_homing_.interrupted = [...];
-  as_homing_.publishFeedback(feedback_homing_);
-  result_homing_.successful = [...];
-   as_homing_.isPreemptedRequested();
+  homingFeedback_.interrupted = [...];
+  homingActionServer_.publishFeedback(homingFeedback_);
+  homingResult_.successful = [...];
+   homingActionServer_.isPreemptedRequested();
   */   
-  as_homing_.setSucceeded(result_homing_); 
+  homingActionServer_.setSucceeded(homingResult_);
 }
 //Publisher
 bool ExampleNode::update(const any_worker::WorkerEvent& event)
 {  
-  pub_joint_state_.publish(msg_joint_state_);
-  pub_device_state_.publish(msg_device_state_);
+  jointStatesPublisher_.publish(extendedJointStates_);
+  deviceStatePublisher_.publish(extendedDeviceStates_);
 
   return true;
 }
